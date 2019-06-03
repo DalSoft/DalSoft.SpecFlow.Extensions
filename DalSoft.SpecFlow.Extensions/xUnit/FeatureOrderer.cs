cbbc8cfa6623208aaee1cc6acc3b704e9a6ac527
Darran <darran@dalsoft.co.uk>
2019-06-03 12:44:01 +0100

    Initial work

---
+++ DalSoft.SpecFlow.Extensions/xUnit/FeatureOrderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace DalSoft.SpecFlow.Extensions.xUnit
+{
+    public class FeatureOrderer : ITestCollectionOrderer
+    {
+        public const string TypeName = "DalSoft.SpecFlow.Extensions.xUnit.FeatureOrderer";
+        public const string AssemblyName = "DalSoft.SpecFlow.Extensions";
+
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+        {
+            var sortedList = new SortedList<int, ITestCollection>();
+            var unOrdered = 100000;
+
+            foreach (var testCollection in testCollections)
+            {
+                var featureClass = testCollection.TestAssembly
+                    .Assembly.GetType(testCollection.DisplayName.Replace("Test collection for ", string.Empty))
+                    .ToRuntimeType();
+
+                var traits = TraitHelper.GetTraits(featureClass);
+
+                int order;
+
+                if (traits.Any(_ => _.Value.StartsWith("FeatureOrder")))
+                    order = Convert.ToInt32(traits.Single(_ => _.Value.StartsWith("FeatureOrder")).Value.Replace("FeatureOrder", string.Empty));
+                else
+                {
+                    unOrdered++;
+                    order = unOrdered;
+                }
+
+                sortedList.Add(order, testCollection);
+            }
+
+            return sortedList.Values;
+        }
+    }
+}
---
+++ DalSoft.SpecFlow.Extensions/xUnit/ScenarioOrderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace DalSoft.SpecFlow.Extensions.xUnit
+{
+    public class ScenarioOrderer : ITestCaseOrderer
+    {
+        public const string TypeName = "DalSoft.SpecFlow.Extensions.xUnit.ScenarioOrderer";
+        public const string AssemblyName = "DalSoft.SpecFlow.Extensions";
+
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            var sortedList = new SortedList<int, TTestCase>();
+            var unOrdered = 100000;
+
+            foreach (var testCase in testCases)
+            {
+                int order;
+                if (testCase.Traits.SelectMany(_ => _.Value).Any(_ => _.StartsWith("ScenarioOrder")))
+                    order = Convert.ToInt32(testCase.Traits.SelectMany(_ => _.Value).Single(_ => _.StartsWith("ScenarioOrder")).Replace("ScenarioOrder", string.Empty));
+                else
+                {
+                    unOrdered++;
+                    order = unOrdered;
+                }
+
+                sortedList.Add(order, testCase);
+            }
+
+            return sortedList.Values;
+        }
+    }
+}
